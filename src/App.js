a4c3e33ca8dfb00ce32e98974624b804e09f9546
soimhur <soim@tumblbug.com>
2017-04-18 07:55:24 +0900

    Test again

--- src/App.js
+++ src/App.js
@@ -262,7 +262,6 @@ const StoryCard = styled.div`
   box-shadow: 0 1px 2px 0 rgba(0,0,0,.1);
   background-color: #fff;
   word-break: keep-all;
-  word-wrap: break-word;
 
   > div {
     padding: 2rem;
