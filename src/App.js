29d9bb5d1980ab6ee726c93d74f4505f57948dcd
soimhur <soim@tumblbug.com>
2017-04-18 06:41:55 +0900

    Modify contents

--- src/App.js
+++ src/App.js
@@ -1,129 +1,376 @@
 import React, { Component } from 'react';
-import styled, { ThemeProvider } from 'styled-components';
-import Button from './Button';
-import { truncate, media } from './style-utils';
-import HedronTest from './HedronTest';
+import styled from 'styled-components';
 
-const theme = {
-  tumblbugRed: 'red',
-  mobileBreakpoint: '1000px',
-};
 
-const Container = styled.div`
-  width: 800px;
-  background-color: goldenrod;
-  margin: auto;
-  min-height: 100vh;
-
-  ${media.mobile`
-    width: 100vw;
-  `}
-`;
-
-const CoverImage = styled.div`
-  width: auto;
-  min-height: 400px;
-  min-width: 50%;
-  background-color: red;
-  order: 2;
-
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-right: 2em;
-
-  @media (max-width: ${props => props.theme.mobileBreakpoint}) {
-    width: 100%;
-    order: 1;
-    margin: 0;
-  }
-`;
-
-const ProjectTitle = styled.h1`
-  order: 1;
+const ProjectIntroductionBackground = styled.div`
+  background-color: #fff;
   width: 100%;
-  text-align: center;
-  margin: .5em;
+  padding-bottom: 3rem;
+  overflow-x: hidden;
+`;
+const Container = styled.div`
+  width: 100%;
+  margin: 0 auto;
 
-  @media (max-width: ${props => props.theme.mobileBreakpoint}) {
-    order: 2;
-    text-align: left;
+  @media (min-width: 1080px) {
+    width: 1080px;
+    margin: 0 auto;
   }
 `;
 
-const ProjectInfo = styled.div`
-  margin: .5em;
-  /* 프로젝트 인포는 데스크탑에서 제일 마지막으로 나와야 한다 */
-  order: 3;
-  width: 40%;
-
-  @media (max-width: ${props => props.theme.mobileBreakpoint}) {
-    width: 100%;
-  }
-`;
-
+//introduction
 const ProjectIntroduction = styled.div`
   display: flex;
-  flex-direction: row;
   flex-wrap: wrap;
 `;
 
-const Metrics = styled.div`
-  @media (max-width: ${props => props.theme.mobileBreakpoint}) {
-    display: flex;
-    justify-content: space-around;
-    width: 100%;
+//projectOutline
+const ProjectOutline = styled.div`
+  order: 2;
+  width: 100%;
+
+  display: flex;
+  align-items: center;
+
+  padding-bottom: 1rem;
+
+  @media (min-width:1080px) {
+    padding-bottom: 0;
+  }
+
+
+  @media (min-width: 1080px) {
+    order: 1;
+    height: 300px;
+    padding-bottom: 0;
+  }
+`;
+const ProjectOutlineInner = styled.div`
+  display: block;
+  text-align: left;
+  margin: 0;
+  padding: 1rem;
+
+  @media (min-width: 1080px) {
+    text-align: center;
+    margin: 0 auto;
+  }
+`;
+const Category = styled.div`
+  color: #757575;
+  font-size: 0.9em;
+`;
+const ProjectTitle = styled.h1`
+  font-size: 1.5rem;
+  line-height: 1.5;
+  padding: 0.5rem 0 1rem;
+  margin: 0;
+
+  @media (min-width: 1080px) {
+    font-size: 2.5rem;
+  }
+`;
+
+const Creators = styled.div`
+  position: relative;
+  display: inline-block;
+`;
+
+const CreatorProfileImage = styled.div`
+  background-image: url(https://tumblbug-upi.imgix.net/48d20b1046be5e42ce74e1b992fc886f3e342df3.png?ixlib=rb-1.1.0&w=200&h=250&auto=format%2Ccompress&fit=facearea&facepad=2.0&ch=Save-Data&mask=ellipse&s=2bdc40e93e414edcaa88f983015d7772);
+  background-position: 50% 38%;
+  border-radius: 50%;
+  background-size: cover;
+  width: 20px;
+  height: 20px;
+  margin-right: 5px;
+  float: left;
+  margin-top: -2px;
+`;
+
+// ProjectCoverImage
+const ProjectCoverImage = styled.div`
+  width: 100%;
+  height: auto;
+  order: 1;
+  margin: 0;
+
+  @media (min-width: 1080px) {
+    width: 650px;
+    height: auto;
+    order: 2;
+  }
+`;
+const Img = styled.img`
+  max-width: 100%;
+  height: auto;
+`;
+
+
+
+// RightPart
+const RightColumn = styled.div`
+  width: 100%;
+  order: 3;
+
+  @media (min-width: 1080px) {
+    width: 380px;
+    order: 3;
+    margin-left: 2rem;
+  }
+
+`;
+// FundingStatus
+const FundingStatus = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+
+  padding-bottom: 1rem;
+
+  @media (min-width:1080px) {
+    padding-bottom: 0;
+  }
+
+  @media (min-width: 1080px) {
+    display: block;
+    height: 260px;
+    margin: 0 0 2rem;
   }
 `;
 
 const Metric = styled.div`
-  margin: 0 0.5em;
-  > h2 {
-    font-size: 1.3em;
+  margin: 0 1rem;
+
+  @media (min-width: 1080px) {
+    margin: 0 0 26px;
+  }
+`;
+const StatusTitle = styled.div`
+  font-size: .8rem;
+  margin-bottom: .5rem;
+  @media (min-width: 1080px) {
+    font-size: .9rem;
+  }
+`;
+const StatusValue = styled.div`
+  font-size: 1.2rem;
+  font-weight: 200;
+  @media (min-width: 1080px){
+    font-size: 2.3rem;
+  }
+`;
+const FundingRate = styled.span`
+  font-size: 0.9rem;
+  margin-left: 10px;
+`;
+const Small = styled.span`
+  font-size: .9rem;
+`;
+
+
+const FundingInformation = styled.div`
+  padding: 1rem;
+  font-size: .9rem;
+  color: rgba(0,0,0,.7);
+  background-color: #f7f7f7;
+  border-bottom: 1px solid #efefef;
+  border-top: 1px solid #efefef;
+  margin: 0;
+
+  > div {
+    font-weight: 700;
+    line-height: 2;
+  }
+
+  > span {
+    line-height: 1.5;
+  }
+
+  @media (min-width: 1080px) {
+    margin: 0 0 1rem;
   }
 `;
 
-const BigMetric = styled(Metric)`
-  > h2 {
-    font-size: 1.8em;
+const ProjectButtons = styled.div`
+  order: 5;
+  background-color: #fa6462;
+  padding: 1.5rem;
+`;
+
+
+const ProjectContentsNavigation = styled.div`
+  background-color: #fff;
+  width: 100%;
+  height: 54px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  box-shadow: 0 1px 3px 0 rgba(0,0,0,.1);
+`;
+const NavigationList = styled.ul`
+  float: left;
+  line-height: 54px;
+
+  > li {
+    list-style: none;
+    display: inline-block;
+    margin-right: 20px;
+    color: rgba(0,0,0,.4);
+
   }
 `;
 
-const ProjectDescription = styled.div`
-  ${truncate('300px')}
-  color: #505050;
+const Active = styled.li`
+  color: black;
+  font-weight: 700;
+  border-bottom: 2px solid black;
 `;
+
+const ProjectContentsBackground = styled.div`
+  background-color: #f2f2f2;
+  width: 100%;
+`;
+
+const ProjectContents = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  padding-top: .25rem;
+`;
+const ProjectContentsMainColumn = styled.div`
+  order: 1;
+  width: 100%;
+
+  @media (min-width: 1080px) {
+    width: 650px;
+    margin: .5rem;
+  }
+`;
+
+const ProjectContentsSubColumn = styled.div`
+  order: 2;
+  width: 100%;
+
+  @media (min-width: 1080px) {
+    width: 380px;
+    margin: .5rem;
+  }
+`;
+
+const StoryCard = styled.div`
+  width: 100%;
+  box-shadow: 0 1px 2px 0 rgba(0,0,0,.1);
+  background-color: #fff;
+
+  > div {
+    padding: 3rem;
+    line-height: 1.5;
+  }
+`;
+
+const CreatorCard = styled.div`
+  width: 100%;
+  height: 400px;
+  box-shadow: 0 1px 2px 0 rgba(0,0,0,.1);
+  background-color: #fff;
+`;
+const RewardCardWrapper = styled.div`
+  margin-top: 1rem;
+  width: 100%;
+
+  > span {
+    color: #757575;
+    font-size: 1rem;
+  }
+`;
+const RewardCard = styled.div`
+  margin-top: .5rem;
+  width: 100%;
+  height: 200px;
+  box-shadow: 0 1px 2px 0 rgba(0,0,0,.1);
+  background-color: #fff;
+`;
+
 
 class App extends Component {
   render() {
     return (
-      <ThemeProvider theme={theme}>
-        <Container>
-          <ProjectIntroduction>
-            <ProjectTitle>
-              프로젝트 타이틀
-            </ProjectTitle>
-            <CoverImage> 
-              프로젝트 이미지
-            </CoverImage>
-            <ProjectInfo>
-              <ProjectDescription>
-                You could create a separate component for truncating, but in this case reusing the CSS might not be a bad idea! Instead of hardcoding those lines of code in every component you want to truncate though, you could write a function that does it for you. You could create a separate component for truncating, but in this case reusing the CSS might not be a bad idea! Instead of hardcoding those lines of code in every component you want to truncate though, you could write a function that does it for you.
-              </ProjectDescription>
-              <Metrics>
-                <Metric><h2>모인 금액</h2> 22,161,500원</Metric>
-                <BigMetric><h2>앞으로</h2> 16일</BigMetric>
-                <Metric><h2>후원자</h2> 990명</Metric>
-              </Metrics>
-              <br />
-              <Button primary>밀어주기</Button>
-              <Button>공유하기</Button>
-            </ProjectInfo>
-          </ProjectIntroduction>
-        <br />
-        <HedronTest />
-        </Container>
-      </ThemeProvider>
+      <div>
+        <ProjectIntroductionBackground>
+          <Container>
+            <ProjectIntroduction>
+              <ProjectOutline>
+                <ProjectOutlineInner>
+                  <Category>일러스트레이션</Category>
+                  <ProjectTitle>인천시에서 좋아할 인천 가이드 .뉴인천. 가이드 북 페스티벌
+                  </ProjectTitle>
+                  <Creators>
+                    <CreatorProfileImage></CreatorProfileImage>
+                    <span>MEAL'S PROJECT</span>
+                  </Creators>
+                </ProjectOutlineInner>
+              </ProjectOutline>
+              <ProjectCoverImage>
+                <Img src="https://tumblbug-pci.imgix.net/15f011715a817d74815ffc600fc1964fda6732fe/ea8371d4f40176cb559819145a420a69be713854/cf7a7dba43df2ad09f8693416da9222e5b5c0fe3/45eb97d32ba8d1c95839763bf7e525ff9dbe0f84.jpg?ixlib=rb-1.1.0&w=620&h=465&auto=format%2Ccompress&lossless=true&fit=crop&s=5e337fe597816ae98c76c57e93955a26" alt=""/>
+              </ProjectCoverImage>
+              <RightColumn>
+                <FundingStatus>
+                  <Metric>
+                    <StatusTitle>모인금액</StatusTitle>
+                    <StatusValue>200,000,000<Small>원</Small><FundingRate>577%
+                    </FundingRate></StatusValue>
+                  </Metric>
+                  <Metric>
+                    <StatusTitle>남은시간</StatusTitle>
+                    <StatusValue>10<Small>일</Small></StatusValue>
+                  </Metric>
+                  <Metric>
+                    <StatusTitle>후원자</StatusTitle>
+                    <StatusValue>9,300<Small>명</Small></StatusValue>
+                  </Metric>
+                </FundingStatus>
+                <FundingInformation>
+                  <div>펀딩진행중</div>
+                  <span>목표금액인 1000만원 이상 모이면 결제됩니다. <br /> 결제는 4월 2일에 다같이 진행됩니다</span>
+                </FundingInformation>
+                <ProjectButtons>
+                  <span>밀어주기</span><span>공유하기</span>
+                </ProjectButtons>
+              </RightColumn>
+            </ProjectIntroduction>
+          </Container>
+        </ProjectIntroductionBackground>
+        <ProjectContentsNavigation>
+          <Container>
+            <NavigationList>
+              <li><Active>스토리</Active></li>
+              <li>커뮤니티</li>
+              <li>FAQ</li>
+            </NavigationList>
+          </Container>
+        </ProjectContentsNavigation>
+        <ProjectContentsBackground>
+          <Container>
+            <ProjectContents>
+              <ProjectContentsMainColumn>
+                <StoryCard>
+                  <div>프로젝트 개요 버드힐링모빌은 무게추 원리에 의해서 손잡이를 당기면 날개짓을 하는 반자동 DIY 새모빌입니다. 날개짓의 움직임이 실제 새가 나는 것처럼 너무나 자연스러워 잔잔한 감동을 느끼고 마음을 힐링할 수 있는 기특한 새모빌입니다. 버드힐링모빌은 2016년에 처음 출시된 브랜드이기도 한데요. 오프라인 행사장과 매장에서 날개짓을 직접 보신 분들의 반응이 매우 뜨거웠던 상품이었습니다. 그러나 그 날개짓을 직접 보기 어려운 온라인에서는 홍보 부족과 맞물려 고전을 하기도 했습니다.게다가 초기 제품은 개발일정 때문에 급하게 제작된 관계로 제품의 완성도와 티테일 부분에서 작은 문제점이 드러나기도 했습니다.</div>
+                </StoryCard>
+              </ProjectContentsMainColumn>
+              <ProjectContentsSubColumn>
+                <CreatorCard>
+                </CreatorCard>
+                <RewardCardWrapper>
+                  <span>선택할 수 있는 12개의 선물이 있습니다</span>
+                  <RewardCard />
+                  <RewardCard />
+                  <RewardCard />
+                </RewardCardWrapper>
+              </ProjectContentsSubColumn>
+            </ProjectContents>
+          </Container>
+        </ProjectContentsBackground>
+      </div>
     );
   }
 }
