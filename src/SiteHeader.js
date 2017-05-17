44491af254f30fe4637a50d4e0a29b866eb25e0d
soimhur <soim@tumblbug.com>
2017-05-17 14:06:38 +0900

    Fix center align

--- src/SiteHeader.js
+++ src/SiteHeader.js
@@ -22,12 +22,15 @@ const CenterMenu = styled.div`
   position: absolute;
   left: 0;
   right: 0;
+  top: 0;
+  bottom: 0;
   margin: 0 auto;
   padding: 0 1rem;
   cursor: pointer;
 
   display: flex;
   justify-content: center;
+  align-items: center;
 
   &:hover > div {
     fill: #757575;
