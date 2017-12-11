037d2904a5128e9a1e93cf4efed54abf77afffe6
soimhur <soim@tumblbug.com>
2017-12-11 16:22:24 +0900

    Add font size variation and eng

--- src/TypoRender.js
+++ src/TypoRender.js
@@ -119,6 +119,13 @@ class TypoRender extends Component {
             더 거슬러 올라가 활자 시대의 글꼴은 더 좋은 예입니다.</p>
             
         </Article>
+        <Container>
+          <h1>Because on-line search databases typically contain only abstracts, it is vital to write a complete but concise description of your work to entice potential readers into obtaining a copy of the full paper. This article describes how to write a good computer architecture abstract for both conference and journal papers. Writers should follow a checklist consisting of: motivation, problem statement, approach, results, and conclusions. Following this checklist should increase the chance of people taking the time to obtain and read your complete paper.</h1>
+        </Container>
+        <Article>
+          <h1>The quick brown fox jumps over the lazy dog</h1>
+          <p>출판사의 현업 마케터로서 고민되는 부분이 있다. 종이책을 팔아서 먹고사는 출판사에서 스테디오라는 플랫폼을 통해 뭘 보여 주고 뭘 얻을지에 관해서다. 가장 직접적으로는 발매 예정된 유유의 신간을 미리 볼 기회를 독자에게 드릴 수 있을 것이다. 최대 1/3의 분량까지 책을 미리 보여 드린다거나. 물론 신간이라 한정했지만, 내가 보지 않은 책은 모두 신간인 까닭에, 그간 주목받지 않았던 책이나 반대로 많은 관심을 받았던 책의 일부를 내용으로써 공유한다면, 웹에서의 적극적인 독서를 권장하는 모양을 갖출 것이다. 고민하기에 따라서, 오프라인 책방에서의 경험을 웹상으로 끌어올 가능성도 있지 않을까. 읽는 책과 보는 책, 넘기는 책의 경험이 다른데, 이것을 특별하게 구현할 방법에 관심이 있다. 저자와의 대화를 웹상으로 진행할 수 있다면 그 또한 신선할 것이다. 책을 읽는 재미를 주는 방법 중 효과적인 것이 낭독인데, 웹상으로 낭독 콘텐츠를 게시하는 것도 재밌을 것 같다. 기본적으로는 책을 팔아서 수익을 얻고 지속적인 출판을 이어 나가는 것이 목적인데, 정보 제공과 홍보가 주가 된다면 기존 출판사가 취했던 전략과 크게 다르지 않을 것 같다. 값진 콘텐츠가 무료로도 많이 제공되는 온라인 환경에서라면, 조금 더 독특한 차별점을 마련해야 한다. 독점적인 콘텐츠로 제공한다거나 멤버십의 가격과 구성을 잘 만든다거나 하는 실천적인 노하우를 오늘 이 자리에서 함께 나누어 보고 싶다. </p>
+        </Article>
       </div>
     );
   }
